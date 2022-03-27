7d758325877639a7d9fa58d5c46d52ff31d8421d
rendao1010 <e0257186@u.nus.edu>
2022-03-27 23:16:47 +0800

    updated package.json with dependencies, app.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <h1>Welcome to FunTaskTic!</h1>
-  <router-link to="/addtask">Add Task</router-link> |
-  <router-link to="/managetask">Manage Task</router-link>
-  <router-view/>
+  <link
+    rel="preconnect"
+    href="https://fonts.googleapis.com"
+  >
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin
+  >
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+    rel="stylesheet"
+  >
+  <el-container
+    direction="vertical"
+  >
+    <NavBar />
+    <el-main
+      class="
+    main"
+    >
+      <router-view />
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 
+import NavBar from './components/NavBar.vue'
+
 export default {
   name: 'App',
   components: {
-    
+    NavBar,
   }
 }
 </script>
 
 <style>
+:root {
+  font-family: 'Poppins', sans-serif;
+  --el-color-primary: #063F68 !important;
+}
+
+.info {
+  color: grey;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+header {
+  position: fixed;
+  width: 100%;
+}
+
+button.el-button {
+  font-weight: 600;
+}
+
+.main.el-main {
+  overflow: hidden;
+}
+
 </style>
