64753c7300d3da3a5d7d66af773a757bcf0f50b7
Rayki-Fang <60532392+Rayki-Fang@users.noreply.github.com>
2022-03-28 16:30:50 +0800

    5th commit: adding the profile section that gives extra info

---
+++ src/components/Profile/ProfileSide.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <el-avatar
+      :src="props.user.avatar"
+      :size="200"
+    />
+    <div class="expanded">
+      <typography type="h1">
+        {{ props.user.name }}
+      </typography>
+      <typography type="h2">
+        {{ props.user.title }}
+      </typography>
+      <typography type="title">
+        {{ props.user.location }}
+      </typography>
+      <typography type="title">
+        {{ props.user.gender }}
+      </typography>
+      <div style="height: 16px" />
+      <typography type="title">
+        Division: {{ props.user.division }}
+      </typography>
+      <typography type="title">
+        Level: {{ props.user.level }}
+      </typography>
+    </div>
+  </div>
+</template>
+<script setup>
+  import Typography from '../../components/Typography.vue';
+
+  const props = defineProps({
+    user: {
+      type: Object,
+      default: null,
+    },
+  });
+</script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.container > :not(.expanded) {
+  flex: inherit;
+}
+
+.expanded {
+  flex: 1;
+}
+
+@media screen and (min-width: 768px) and (max-width: 991px) {
+  .container {
+    flex-direction: row;
+  }
+}
+</style>
