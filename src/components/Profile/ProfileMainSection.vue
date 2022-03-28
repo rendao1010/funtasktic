6292a2bf1142d17b56feb2d800ab4cb133e47d53
Rayki-Fang <60532392+Rayki-Fang@users.noreply.github.com>
2022-03-28 16:29:12 +0800

    4th commit: adding the profile main section, prob have another interface for this

---
+++ src/components/Profile/ProfileMainSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <typography type="h2">
+    About me
+  </typography>
+  <div>
+    {{ props.user.aboutMe }}
+  </div>
+  <el-divider />
+  <typography type="h2">
+    Past experience
+  </typography>
+  <div>
+    {{ props.user.pastExperience }}
+  </div>
+  <el-divider />
+  <typography type="h2">
+    Hobbies
+  </typography>
+  <div>
+    {{ props.user.hobbies }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Typography from '../../components/Typography.vue';
+
+const props = defineProps({
+  user: {
+    type: Object,
+    default: null,
+  },
+});
+
+</script>
