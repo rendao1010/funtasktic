ee971f876e400c3932455bf00e7c667f31ec2d6d
abhishekjainz <jabhishek1999@gmail.com>
2022-03-30 02:04:34 +0800

    Added TaskComment and TaskListCard components

---
+++ src/components/Task/TaskComment.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-space :size="8">
+      <el-avatar
+        :src="props.author.avatar"
+        :size="24"
+      />
+      <p>
+        {{ props.author.name }}
+      </p>
+    </el-space>
+    <p class="content">
+      {{ props.content }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  author: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+  content: {
+    type: String,
+    default: '',
+  }
+})
+</script>
+
+<style scoped>
+  p {
+    margin: 0;
+  }
+
+  .content {
+    margin-top: 8px;
+  }
+</style>
