505f18877a63fb4cdabe58590ce6de7c99b51e9b
Abhishek <65821265+abhishekjainz@users.noreply.github.com>
2022-04-09 22:05:59 +0800

    Create Meetings.vue

---
+++ src/views/Meetings.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-row justify="center">
+    <el-col
+      :span="24"
+      :lg="18"
+    >
+      <meetings />
+    </el-col>
+  </el-row>
+</template>
+<script setup>
+  import Meetings from '../components/meetings/Meetings.vue';
+</script>
