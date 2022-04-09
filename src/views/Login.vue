6fffa18a4e3f237d8d378ebca1dc90ba73be36ed
Rayki-Fang <60532392+Rayki-Fang@users.noreply.github.com>
2022-04-09 19:34:24 +0800

    updated the Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,11 @@
 <template>
-  <el-card class="card">
-    <LoginForm />
-  </el-card>
+  <div
+    class="background"
+  >
+    <el-card class="card">
+      <LoginForm />
+    </el-card>
+  </div>
 </template>
 
 <script setup>
@@ -12,6 +16,30 @@
   .card {
     width: 100%;
     max-width: 400px;
-    margin-left: max(0px, calc(calc(100vw - 400px) * .12));
   }
+
+  .background {
+    display: flex;
+    align-items: center;
+  }
+
+  @media screen and (min-width: 1260px) {
+    .background {
+      background-image: url('~@/assets/login_banner.png');
+      background-size: cover-fit; 
+      background-position: right;
+      background-repeat: no-repeat;
+    }
+
+    .card {
+      margin-left: max(0px, calc(calc(100vw - 400px) * .12));
+    }
+  }
+
+  @media screen and (max-width: 1260px) {
+    .background {
+      justify-content: center;
+    }
+  }
+   
 </style>
