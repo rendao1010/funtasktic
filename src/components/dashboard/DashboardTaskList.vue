693e11411e7898c9ed5e7ec221d1ad4bcfb40f06
Zhao Wei Lo <lozhaowei@gmail.com>
2022-04-09 22:31:49 +0800

    added backend for DashboardTaskList

--- src/components/dashboard/DashboardTaskList.vue
+++ src/components/dashboard/DashboardTaskList.vue
@@ -5,20 +5,46 @@
     </typography>
     <div style="height: 24px" />
     <el-space
+      v-if="!loading"
       style="width: 100%"  
       direction="vertical"
       alignment="stretch"
-      fill="true"
+      :fill="true"
     >
       <dashboard-task-item
         v-for="task in props.tasks"
         :key="task"
-        :title="task.name"
-        :subtitle="task.date"
-        :tags="task.tags"
-        :color="task.color"
+        :task="task"
+        :on-task-click="props.onTaskClick"
       />
+      <typography
+        v-if="props.tasks.length === 0"
+        type="title"
+        style="color: #909399"
+      >
+        No tasks to show
+      </typography>
     </el-space>
+    
+    <div
+      v-else
+    >
+      <el-skeleton
+        v-for="i in [1,2,3]"
+        :key="i"
+      >
+        <template #template>
+          <el-skeleton-item
+            variant="text"
+            style="margin-right: 16px"
+          />
+          <el-skeleton-item
+            variant="text"
+            style="width: 30%; margin-bottom: 16px;"
+          />
+        </template>
+      </el-skeleton>
+    </div>
   </el-card>
 </template>
 <script setup>
@@ -26,6 +52,7 @@ import { ref } from 'vue';
 
 import DashboardTaskItem from '../dashboard/DashboardTaskItem.vue';
 import Typography from '../../components/Typography.vue';
+import { ElSkeleton, ElSkeletonItem } from 'element-plus';
 
 const props = defineProps({
   tasks: {
@@ -33,6 +60,14 @@ const props = defineProps({
     default() {
       return [];
     },
+  },
+  onTaskClick: {
+    type: Function,
+    default: () => {},
+  },
+  loading: {
+    type: Boolean,
+    default: false,
   },
 });
 </script>
