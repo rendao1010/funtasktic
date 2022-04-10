a187121baf246f13ba4762c4dae11ad7c8906f33
Zhao Wei Lo <lozhaowei@gmail.com>
2022-04-10 15:42:32 +0800

    added analytics component

---
+++ src/components/analytics/ProgressIndicatorCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <typography type="h1">
+      {{ props.title }}
+    </typography>
+    <typography type="h2">
+      {{ props.subtitle }}
+    </typography>
+    <typography type="p">
+      {{ props.completionCount }}
+    </typography>
+  </div>
+</template>
+<script setup>
+  import Typography from '../Typography.vue';
+
+  const props = defineProps(
+    {
+      title: {
+        type: String,
+        default: '',
+      },
+      subtitle: {
+        type: String,
+        default: '',
+      },
+      completionCount: {
+        type: Number,
+        default: 0,
+      },
+    }
+  );
+</script>
